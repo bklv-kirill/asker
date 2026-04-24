ff64b25e29730d559c2e7f6dc27677d3c38b13e1
bklv-kirill <bklv.kirill@yandex.ru>
2026-04-24 22:52:39 +0500

    [MODIFY]:main-loads-config-on-startup

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
+
+	"github.com/bklv-kirill/asker/internal/config"
 )
 
 func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("load config: %v", err)
+	}
+
+	fmt.Printf("starting %s (%s)\n", cfg.AppName, cfg.BotName)
+
 	ticker := time.NewTicker(3 * time.Second)
 	defer ticker.Stop()
 
