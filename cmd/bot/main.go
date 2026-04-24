2c9e045eab3283264d5ab8d285bcfba8afab6f34
bklv-kirill <bklv.kirill@yandex.ru>
2026-04-24 23:10:09 +0500

    [MODIFY]:main-runs-telegram-bot-with-graceful-shutdown

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
+	"context"
 	"log"
-	"time"
+	"os/signal"
+	"syscall"
 
 	"github.com/bklv-kirill/asker/internal/config"
+	"github.com/bklv-kirill/asker/internal/telegram"
 )
 
 func main() {
 	cfg := config.Load()
 
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
 	log.Printf("starting %s (%s)", cfg.AppName, cfg.BotName)
 
-	ticker := time.NewTicker(3 * time.Second)
-	defer ticker.Stop()
-
-	for range ticker.C {
-		log.Println("working...")
+	tg := telegram.NewTelegramBot(cfg.TokenBotToken, cfg.BotName)
+	if err := tg.Start(ctx); err != nil {
+		log.Fatalf("telegram: %v", err)
 	}
+
+	log.Printf("shutdown complete")
 }
